e744cac4071f39ccc2938a122351cd12c861354c
Ibti34 <ibtisamkedir076@gmail.com>
2026-02-06 07:09:15 +0300

    to check weather to check if laravel works

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,11 @@
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
-            @if (Route::has('login'))
+          
+          <h1 style="background:red;color:white;padding:30px;">
+  THIS IS BLADE – IF YOU SEE THIS, LARAVEL WORKS
+</h1>
+  @if (Route::has('login'))
                 <nav class="flex items-center justify-end gap-4">
                     @auth
                         <a
