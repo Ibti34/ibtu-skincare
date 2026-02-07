7260d5e3b48c89e6e1f94ab19f22adda2d359df1
Ibti34 <ibtisamkedir076@gmail.com>
2026-02-07 07:44:30 +0300

    replace old code od app serviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\URL; // <--- Add this line
+
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,10 +17,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
-
-        // ✅ Add this block to fix the "naked" layout on Railway
-        if (config('app.env') === 'production') {
-            URL::forceScheme('https');
-        }
     }
 }
